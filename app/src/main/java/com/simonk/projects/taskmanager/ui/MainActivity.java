fe8acf4061d15dfeee457f41a373ab3f9c9697ab
simon <sam24free@gmail.com>
2019-04-21 15:09:26 +0300

    Tabs introduced. Apps, Blacklist, Terminal. Process fragment add. Process logic moved from activity to fragment

--- app/src/main/java/com/simonk/projects/taskmanager/ui/MainActivity.java
+++ app/src/main/java/com/simonk/projects/taskmanager/ui/MainActivity.java
@@ -19,15 +19,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
 import androidx.databinding.ViewDataBinding;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
 import com.simonk.projects.taskmanager.entity.ProcessInfo;
 import com.simonk.projects.taskmanager.repository.ProcessRepository;
 import com.simonk.projects.taskmanager.ui.process.ChangeDetailsDialog;
 import com.simonk.projects.taskmanager.ui.process.CleanedDialog;
+import com.simonk.projects.taskmanager.ui.process.ProcessFragment;
 import com.simonk.projects.taskmanager.ui.process.viewmodels.ProcessViewModel;
 import com.simonk.projects.taskmanager.ui.util.ObjectListAdapter;
 import com.simonk.projects.taskmanager.R;
@@ -38,12 +44,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends BindingActivity
-        implements CleanedDialog.OnCleaned, ChangeDetailsDialog.OnChanged {
-
-    private ProcessAdapter processAdapter;
-
-    private ProcessViewModel mProcessViewModel;
+public class MainActivity extends BindingActivity {
 
     @Override
     public ActivityMainBinding getBinding() {
@@ -59,155 +60,51 @@ public class MainActivity extends BindingActivity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mProcessViewModel = ViewModelProviders.of(this).get(ProcessViewModel.class);
+        ViewPager viewPager = getBinding().viewPager;
+        viewPager.setOffscreenPageLimit(2);
+        setupViewPager(viewPager);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        RecyclerView recyclerView = findViewById(R.id.activity_main_recycler);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
-        processAdapter = new ProcessAdapter();
-        recyclerView.setAdapter(processAdapter);
-
-        findViewById(R.id.clear_all).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CleanedDialog cleanedDialog = new CleanedDialog();
-                cleanedDialog.show(getSupportFragmentManager(), "CleanedDialog");
-            }
-        });
-
-        processAdapter.setItemClickListener(new ProcessAdapter.ProcessAdapterViewHolder.OnClickListener() {
-            @Override
-            public void onClick(View v, ProcessInfo info) {
-                ViewGroup detailsView = (ViewGroup)
-                        LayoutInflater.from(MainActivity.this).inflate(R.layout.process_list_item_details, null);
-                if (((FrameLayout) v.findViewById(R.id.process_list_item_details)).getChildCount() == 0) {
-                    ((TextView) detailsView.findViewById(R.id.priority)).setText("Priority: " + info.getPriority());
-                    ((TextView) detailsView.findViewById(R.id.status)).setText("Enabled: " + info.isStatus());
-                    ((TextView) detailsView.findViewById(R.id.uid)).setText("Uid: " + info.getUid());
-                    ((TextView) detailsView.findViewById(R.id.min_sdk)).setText("Target sdk: " + info.getMinSdk());
-                    ((TextView) detailsView.findViewById(R.id.description)).setText("Description: " + info.getDescription());
-                    ((FrameLayout) v.findViewById(R.id.process_list_item_details)).addView(detailsView);
-                    ((Button) detailsView.findViewById(R.id.kill)).setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            mProcessViewModel.getRepository().killProcess(MainActivity.this, info);
-                            updateUi();
-                        }
-                    });
-                } else {
-                    ((FrameLayout) v.findViewById(R.id.process_list_item_details)).removeAllViews();
-                }
-            }
-
-            @Override
-            public boolean onLongClick(ProcessInfo info) {
-                Bundle bundle = new Bundle();
-                bundle.putSerializable(ChangeDetailsDialog.PROCESS_INFO_ARG, info);
-                ChangeDetailsDialog changeDetailsDialog = new ChangeDetailsDialog();
-                changeDetailsDialog.setArguments(bundle);
-                changeDetailsDialog.show(getSupportFragmentManager(), "ChangeDetailsDialog");
-                return false;
-            }
-        });
-
-        mProcessViewModel.getAllProcessInfo().observe(this, this::updateProcessAdapter);
+        TabLayout tabLayout = getBinding().tabLayout;
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        updateUi();
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(ProcessFragment.newInstance(),
+                "Apps");
+        adapter.addFragment(ProcessFragment.newInstance(),
+                "BlackList");
+        adapter.addFragment(ProcessFragment.newInstance(),
+                "Terminal");
+        viewPager.setAdapter(adapter);
     }
 
-    private void updateUi() {
-        float allMemory = mProcessViewModel.getTotalMemory();
-        float availMemory = mProcessViewModel.getAvailableMemory();
-        ((TextView)findViewById(R.id.all_memory)).setText("" + allMemory + "G");
-        ((TextView)findViewById(R.id.free_memory)).setText("" + availMemory + "G");
-        ((TextView)findViewById(R.id.percent_memory)).setText("" + (int)(availMemory * 100 / allMemory) + "%");
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
 
-        mProcessViewModel.forceUpdateProcessInfo();
-    }
-
-    private void updateProcessAdapter(List<ProcessInfo> processInfoList) {
-        processAdapter.resolveActionChange(() -> {
-            processAdapter.setItemsList(processInfoList);
-        });
-    }
-
-    @Override
-    public void onCleaned() {
-        updateUi();
-    }
-
-    @Override
-    public void onChanged(ProcessInfo info, int priority) {
-        mProcessViewModel.getRepository().changeProcessPriority(this, info, priority);
-        updateUi();
-    }
-
-    private static class ProcessAdapter extends ObjectListAdapter<ProcessInfo, ProcessAdapter.ProcessAdapterViewHolder> {
-
-        private ProcessAdapterViewHolder.OnClickListener mItemClickListener;
-
-        @NonNull
-        @Override
-        public ProcessAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-            LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
-            View v = inflater.inflate(R.layout.process_list_item, viewGroup, false);
-
-            return new ProcessAdapterViewHolder(v, mItemClickListener);
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull ProcessAdapterViewHolder processAdapterViewHolder, int i) {
-            processAdapterViewHolder.bind(getItem(i));
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
         }
 
-        public void setItemClickListener(ProcessAdapterViewHolder.OnClickListener onClickListener) {
-            mItemClickListener = onClickListener;
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
         }
 
-        public static class ProcessAdapterViewHolder extends RecyclerView.ViewHolder {
-
-            private TextView mName;
-            private ImageView mImage;
-            private TextView mPackage;
-
-            private ProcessInfo mItem;
-
-            public interface OnClickListener {
-                void onClick(View v, ProcessInfo ppackage);
-                boolean onLongClick(ProcessInfo info);
-            }
-
-            public ProcessAdapterViewHolder(@NonNull View itemView, OnClickListener onClickListener) {
-                super(itemView);
-                mName = itemView.findViewById(R.id.process_list_item_text);
-                mImage = itemView.findViewById(R.id.process_list_item_logo);
-                mPackage = itemView.findViewById(R.id.process_list_item_package);
-                itemView.setOnClickListener(v -> {
-                    if (onClickListener != null) {
-                        onClickListener.onClick(v, mItem);
-                    }
-                });
-                itemView.setOnLongClickListener(v -> {
-                    if (onClickListener != null) {
-                        return onClickListener.onLongClick(mItem);
-                    }
-                    return false;
-                });
-            }
-
-            public void bind(ProcessInfo info) {
-                mItem = info;
-                mName.setText(info.getText());
-                mImage.setImageDrawable(info.getImage());
-                mPackage.setText(info.getPpackage());
-            }
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
         }
 
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
---
+++ app/src/main/java/com/simonk/projects/taskmanager/ui/process/ProcessFragment.java
@@ -0,0 +1,148 @@
+package com.simonk.projects.taskmanager.ui.process;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.simonk.projects.taskmanager.R;
+import com.simonk.projects.taskmanager.databinding.FragmentProcessBinding;
+import com.simonk.projects.taskmanager.entity.ProcessInfo;
+import com.simonk.projects.taskmanager.ui.process.viewmodels.ProcessViewModel;
+
+import java.util.List;
+
+public class ProcessFragment extends Fragment
+        implements CleanedDialog.OnCleaned, ChangeDetailsDialog.OnChanged {
+
+    private RecyclerView mAppsRecyclerView;
+    private Button mClearAllButton;
+    private TextView mAllMemory;
+    private TextView mFreeMemory;
+    private TextView mPercentMemory;
+
+    private ProcessAdapter mProcessAdapter;
+
+    private ProcessViewModel mViewModel;
+
+    public static ProcessFragment newInstance() {
+        ProcessFragment fragment = new ProcessFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
+        View root = bindingView(inflater, container);
+
+        mAppsRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext(),
+                RecyclerView.VERTICAL, false));
+        mProcessAdapter = new ProcessAdapter();
+        mAppsRecyclerView.setAdapter(mProcessAdapter);
+
+        mClearAllButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CleanedDialog cleanedDialog = new CleanedDialog();
+                cleanedDialog.setTargetFragment(ProcessFragment.this, 0);
+                cleanedDialog.show(requireFragmentManager(), "CleanedDialog");
+            }
+        });
+
+        mProcessAdapter.setItemClickListener(new ProcessAdapter.ProcessAdapterViewHolder.OnClickListener() {
+            @Override
+            public void onClick(View v, ProcessInfo info) {
+                ViewGroup detailsView = (ViewGroup)
+                        LayoutInflater.from(requireContext()).inflate(R.layout.process_list_item_details, null);
+                if (((FrameLayout) v.findViewById(R.id.process_list_item_details)).getChildCount() == 0) {
+                    ((TextView) detailsView.findViewById(R.id.priority)).setText("Priority: " + info.getPriority());
+                    ((TextView) detailsView.findViewById(R.id.status)).setText("Enabled: " + info.isStatus());
+                    ((TextView) detailsView.findViewById(R.id.uid)).setText("Uid: " + info.getUid());
+                    ((TextView) detailsView.findViewById(R.id.min_sdk)).setText("Target sdk: " + info.getMinSdk());
+                    ((TextView) detailsView.findViewById(R.id.description)).setText("Description: " + info.getDescription());
+                    ((FrameLayout) v.findViewById(R.id.process_list_item_details)).addView(detailsView);
+                    ((Button) detailsView.findViewById(R.id.kill)).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            mViewModel.getRepository().killProcess(requireContext(), info);
+                            updateUi();
+                        }
+                    });
+                } else {
+                    ((FrameLayout) v.findViewById(R.id.process_list_item_details)).removeAllViews();
+                }
+            }
+
+            @Override
+            public boolean onLongClick(ProcessInfo info) {
+                Bundle bundle = new Bundle();
+                bundle.putSerializable(ChangeDetailsDialog.PROCESS_INFO_ARG, info);
+                ChangeDetailsDialog changeDetailsDialog = new ChangeDetailsDialog();
+                changeDetailsDialog.setArguments(bundle);
+                changeDetailsDialog.show(requireFragmentManager(), "ChangeDetailsDialog");
+                return false;
+            }
+        });
+
+        mViewModel = ViewModelProviders.of(this).get(ProcessViewModel.class);
+        mViewModel.getAllProcessInfo().observe(this, this::updateProcessAdapter);
+
+        return root;
+    }
+
+    private View bindingView(LayoutInflater inflater, ViewGroup container) {
+        FragmentProcessBinding binding
+                = DataBindingUtil.inflate(inflater, R.layout.fragment_process, container, false);
+
+        mAppsRecyclerView = binding.activityMainRecycler;
+        mClearAllButton = binding.clearAll;
+        mAllMemory = binding.allMemory;
+        mPercentMemory = binding.percentMemory;
+        mFreeMemory = binding.freeMemory;
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUi();
+    }
+
+    private void updateUi() {
+        float allMemory = mViewModel.getTotalMemory();
+        float availMemory = mViewModel.getAvailableMemory();
+        mAllMemory.setText("" + allMemory + "G");
+        mFreeMemory.setText("" + availMemory + "G");
+        mPercentMemory.setText("" + (int)(availMemory * 100 / allMemory) + "%");
+
+        mViewModel.forceUpdateProcessInfo();
+    }
+
+    private void updateProcessAdapter(List<ProcessInfo> processInfoList) {
+        mProcessAdapter.resolveActionChange(() -> {
+            mProcessAdapter.setItemsList(processInfoList);
+        });
+    }
+
+    @Override
+    public void onCleaned() {
+        updateUi();
+    }
+
+    @Override
+    public void onChanged(ProcessInfo info, int priority) {
+        mViewModel.getRepository().changeProcessPriority(requireContext(), info, priority);
+        updateUi();
+    }
+
+}
